a4cf1c86c14eab91a6ddc7532cc6e76ff811831d
SilbaB <murkomenbasil@gmail.com>
2024-09-08 14:44:02 +0300

    finale

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ st.write("StudyTimeWeekly: Weekly study time in hours, ranging from 0 to 20.")
 st.write("Tutoring: Tutoring status, where 0 indicates No and 1 indicates Yes.")
 st.write("ParentalSupport: The level of parental support, coded as follows,0:None,1:Low,2:Moderate,3:High,4:very high")
 st.write("ParentalEducation: The education level of the parents, coded as follows:0:None,1:High School,2: Some College,:3:Bachelor's,4:higher")
+st.write("Extracurricular: Participation in extracurricular activities, where 0 indicates No and 1 indicates Yes.")
+st.write("Sports: Participation in sports, where 0 indicates No and 1 indicates Yes.")
+st.write("Music: Participation in music activities, where 0 indicates No and 1 indicates Yes.")
+st.write("Volunteering: Participation in volunteering, where 0 indicates No and 1 indicates Yes.")
+st.write("Absences: Number of absences during the school year, ranging from 0 to 30.")
+st.write("Classification of students' grades,0:'A',1:'B',2:'C',3: 'D', 4: 'F'  ")
+
 
 age = st.selectbox("Age", [15,16,17,18])
 Gender = st.selectbox("	Gender", [0,1])
